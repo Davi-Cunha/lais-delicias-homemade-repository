e0f67793b0bdbd6497d3818b746f5d979cb3dd20
MANGA-ESTRANHA <davipccunhasilva@hotmail.com>
2017-11-27 01:57:29 -0200

    nav bar created

--- index.php
+++ index.php
@@ -3,13 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <title>Laís Lins - Delícias Homemade</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/08e121ba13.js"></script>
 </head>
 <body>
-    <header>
+<header>
+    <nav>
+        <div class="container">
+            <div class="row">
 
+                <ul class="social">
+                    <li><a><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+                    <li><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                </ul>
 
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h1>Laís Lins</h1>
+                </div>
+                <div class="col-md-8">
+                    <ul class="menu">
+                        <li><a>Home</a></li>
+                        <li><a>Contatos</a></li>
 
-    </header>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
 
 </body>
 </html>
