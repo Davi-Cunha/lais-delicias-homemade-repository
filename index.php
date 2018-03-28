ce5ef2259f8291be849629488954b1736dc216f8
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-03-28 03:15:14 -0300

    Layout redesign

--- index.php
+++ index.php
@@ -10,36 +10,70 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+    <script type="text/javascript" src="js/general.js"> </script>
 </head>
-<body>
+<body class="body-class" onscroll="onScrollTopDisplayNone()">
 <header class="title-menu">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="title-img-logo">
-                    <img src="img/logo.png">
-                </div>
+                <nav class="nav-bar-cart">
+                    <a class="float-right cart" href="#"><i class="fas fa-shopping-cart"></i> Carrinho</a>
+                </nav>
             </div>
             <div class="col-md-6">
-                <ul class="list-inline float-right title-menu-left">
+                <ul class="list-inline title-menu-left">
                     <li class="list-inline-item"><a href="#">Home</a></li>
                     <li class="list-inline-item"><a href="#">Galeria</a></li>
                     <li class="list-inline-item"><a href="#">Encomendas</a></li>
                 </ul>
             </div>
             <div class="col-md-6">
-                <ul class="list-inline float-left title-menu-right">
+                <ul class="list-inline float-right title-menu-right">
                     <li class="list-inline-item"><a href="#">Contato</a></li>
-                    <li class="list-inline-item"><a href="#">Contato</a></li>
+                    <li class="list-inline-item"><a href="#">Onde?</a></li>
                     <li class="list-inline-item"><a href="#">Sobre</a></li>
                 </ul>
             </div>
         </div>
     </div>
-
-
 </header>
 
+<section class="galeria-section">
+    <img src="./img/bg.png">
+    <div class="container">
+        <header>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-center">Galeria</h1>
+                </div>
+            </div>
+        </header>
+
+    </div>
+
+
+</section>
+
+<section class="encomenda-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Contato</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="contato-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Contato</h1>
+            </div>
+        </div>
+    </div>
+</section>
 
 </body>
 </html>
---
+++ js/general.js
@@ -0,0 +1,19 @@
+function onScrollTopDisplayNone() {
+    var y = window.pageYOffset;
+    var cart = document.querySelector(".cart");
+    var ulMenuLeft = document.querySelector(".title-menu-left");
+    var ulMenuRight = document.querySelector(".title-menu-right");
+    var galeria = document.querySelector(".galeria-section");
+    console.log("y = " + y);
+    if (y > 800) {
+        cart.style.display = "none";
+        ulMenuLeft.style.display = "none";
+        ulMenuRight.style.display = "none";
+    } else {
+        cart.style.display = "block";
+        ulMenuLeft.style.display = "block";
+        ulMenuRight.style.display = "block";
+    }
+
+
+}
