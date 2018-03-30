3337a88a220f455cf757e9d54b703f23c25173f4
Davi-Cunha <davi123pc>
2018-03-30 17:38:22 -0300

    Divide the code section into separated includes.

--- index.php
+++ index.php
@@ -1,108 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-bt">
 <head>
-    <meta charset="UTF-8">
-    <title>Laís Lins - Delícias Homemade</title>
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
-            integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ"
-            crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
-    <script type="text/javascript" src="js/general.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <?php include("includes/link-path-include.php"); ?>
 </head>
 <body class="body-class" onscroll="onScrollTopDisplayNone()">
-<header id="home" class="title-menu">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav class="nav-bar-cart">
-                    <a class="float-right cart" href="#"><i class="fas fa-shopping-cart"></i> Carrinho</a>
-                </nav>
-            </div>
-            <div class="col-md-6">
-                <nav class="nav">
-                    <ul class="list-inline title-menu-left">
-                        <li class="list-inline-item"><a href="#home">Home</a></li>
-                        <li class="list-inline-item"><a href="#galeria">Galeria</a></li>
-                        <li class="list-inline-item"><a href="#encomenda">Encomendas</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-md-6">
-                <nav class="nav">
-                    <ul class="list-inline float-right title-menu-right">
-                        <li class="list-inline-item"><a href="#contato">Contato</a></li>
-                        <li class="list-inline-item"><a href="#">Onde?</a></li>
-                        <li class="list-inline-item"><a href="#">Sobre</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
 
-<section id="galeria" class="galeria-section">
-    <div class="container">
-        <header>
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="text-center">Galeria</h1>
-                </div>
-            </div>
-        </header>
+<?php include("includes/title-menu-include.php"); ?>
 
-    </div>
+<?php include("includes/galeria-section-include.php"); ?>
 
+<?php include("includes/encomenda-section-include.php"); ?>
 
-</section>
+<?php include("includes/contato-section-include.php"); ?>
 
-<section id="encomenda" class="encomenda-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Encomendas</h1>
-            </div>
-        </div>
-    </div>
-</section>
+<?php include("includes/footer-section-include.php"); ?>
 
-<section id="contato" class="contato-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Contato</h1>
-            </div>
-        </div>
-    </div>
-</section>
+<?php include("includes/toTopButton-include.php"); ?>
 
-<footer class="footer-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Footer</h1>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php include("includes/link-path-smooth-scroll-include.php"); ?>
 
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <nav class="nav">
-                <a class="toTopButton" href="#home"><i class="fas fa-arrow-up"></i></a>
-            </nav>
-        </div>
-    </div>
-</div>
-
-<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-localScroll/2.0.0/jquery.localScroll.min.js"></script>
-<script src="js/general.js"></script>
 </body>
 </html>
